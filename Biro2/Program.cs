18a5f51d478f71866bbb38f4dfa129c2e40e6702
egoro <egoro@DESKTOP-AQ8OOQN>
2023-06-19 12:55:20 +0200

    Add project files.

---
+++ Biro2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int N = System.Convert.ToInt32(System.Console.ReadLine());
+        int[] amountFish = new int[N];
+        int[] indexTrue = new int[N];
+        int count = 0;
+        for (int i = 0; i < N; i++)
+        {
+            amountFish[i] = System.Convert.ToInt32(System.Console.ReadLine());
+
+            if (amountFish[i] < 5)
+            {
+                count++;
+                indexTrue[i] = i+1;
+            }
+            else
+            {
+                indexTrue[i] = 0;
+            }
+        }
+        Console.Write(count);
+        for (int i = 0; i < N; i++)
+        {
+            if (indexTrue[i] != 0)
+                Console.Write($" {indexTrue[i]}");
+        }
+
+    }
+}
